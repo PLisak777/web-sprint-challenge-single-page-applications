44c0c736701b4db7ce78d851b1b595717c4d915e
Phil Lisak <phil.lisak@gmail.com>
2020-08-14 18:25:02 -0700

    Added dropdown for sizes

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,11 +3,22 @@ import * as yup from 'yup';
 import axios from 'axios';
 
 const Form = props => {
+// Setup state for Form elements (sauce, toppings, instructions)
+
 
 
     return (
         <div>
-            
+            <form /* onSubmit={submitForm} */>
+                <select id='sizes' name='sizes' data-cy='sizes' /* onChange={inputChange} */>
+                    <option>Small</option>
+                    <option>Medium</option>
+                    <option>Large</option>
+                </select>
+            </form>
+
         </div>
     )
 }
+
+export default Form;
