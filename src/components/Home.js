c848c8704b0863ad1b188424855678db90f61664
Phil Lisak <phil.lisak@gmail.com>
2020-08-15 17:43:27 -0700

    Styling for Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import { Link, Router, Switch, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+import img from '../Assets/Pizza.jpg';
 
 const Home = (props) => {
     const history = useHistory();
 
+    const Banner = styled.div`
+        border: 1px solid red;
+        background: url(${img});
+        background-size: cover no-repeat;
+        height: 450px;
+        display: grid;
+        place-items: center;
+    `
+
+    const Button = styled.button`
+        background: white;
+        color: black;
+        font-size: 1em;
+        margin: 1em;
+        padding: 0.25em 1em;
+        border: 2px solid black;
+        border-radius: 3px;
+    `
+
 return (
 
-<div className='banner-top'>
-    <img src='Assets\Pizza.jpg' />
-    <p>Your favorite food, delivered while coding</p>
-    <button 
+<Banner className='banner-top'>
+    <h1>Your favorite food, delivered while coding</h1>
+    <Button 
     className='pizzabtn' 
     onClick={() => history.push('/pizza')}>
         Pizza Time!
-    </button>
-</div>
+    </Button>
+</Banner>
 
 
 
